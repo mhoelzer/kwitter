e7e93957c882d15511a5002d3e3f4473c29cd73c
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-29 11:42:58 -0500

    Updates

---
+++ precache-manifest.c92d31a963514d623efbb648543afeea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa57c88fcbd9adee9ddd",
+    "url": "/kwitter/static/css/main.e863affc.chunk.css"
+  },
+  {
+    "revision": "aa57c88fcbd9adee9ddd",
+    "url": "/kwitter/static/js/main.aa57c88f.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/kwitter/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "1bef2d238c070dadcd30",
+    "url": "/kwitter/static/js/runtime~main.1bef2d23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kwitter/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f40a8ee31d44fad4943e6adfd5c9d61b",
+    "url": "/kwitter/index.html"
+  }
+];
