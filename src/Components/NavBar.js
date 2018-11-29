1510f673dd103532093afa479ec9d44a4b3b1084
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-29 16:07:13 -0500

    added some components

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>NavBar</div>
+        );
+    };
+};
+
+export default NavBar;
