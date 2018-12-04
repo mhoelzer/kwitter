d6ea906d9cb17599e9fe599e566acb93748730a0
Michelle Hoelzer <hoelzermm@gmail.com>
2018-12-04 16:57:25 -0500

    login layout done; needs functionality

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Button, Card, Form, Header, Image, Input, } from "semantic-ui-react";
+import { Button, Form, Grid, Header, Input, Segment } from "semantic-ui-react";
 
 class Login extends Component {
     state = {
@@ -22,22 +22,36 @@ class Login extends Component {
     render() {
         const { username, password } = this.state;
         return (
-            // <Card>
-                <Form>
-                    <Header as="h1">Login</Header>
-                    <p>Username:
-                    {/* make sure to add the onchanges otherwise value wont work */}
-                        <input placeholder="Username" type="text" className="loginInputs" autoFocus onChange={this.handleUsernameChange} value={username}></input>
-                    </p>
-                    <p>Password:
-                    <input placeholder="Password" type="password" className="loginInputs" onChange={this.handlePasswordChange} value={password}></input>
-                    </p>
-                    {/* change the tp="" if needed */}
-                    <Link to="/profile">
-                        <button>Login!</button>
-                    </Link>
-                </Form>
-            // </Card>
+            <React.Fragment className="registerForm">
+                <style>{`
+                    body > div,
+                    body > div > div,
+                    body > div > div > div.registerForm {
+                        height: 100%;
+                    }
+                `}</style>
+                <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <Header as="h1" color="blue" textAlign="center">Login</Header>
+                        <Form size="large">
+                            <Segment stacked>
+                                <Form.Field label="Username:" required placeholder="Username" type="text" control={Input} autoFocus onChange={this.handleUsernameChange} />
+                                <Form.Field label="Password:" required placeholder="Password" type="password" control={Input} onChange={this.handlePasswordChange} />
+                                <Button.Group>
+                                    <Link to="/profile"> {/* change the to="" if needed */}
+                                        <Button onClick={this.handleRegister} positive size="large">Login to Your Account!</Button>
+                                        {/* <div>{this.props.result}</div> */}
+                                    </Link>
+                                    <Button.Or />
+                                    <Link to="/register">
+                                        <Button size="large">Don't Have an Account? Register Here!</Button>
+                                    </Link>
+                                </Button.Group>
+                            </Segment>
+                        </Form>
+                    </Grid.Column>
+                </Grid>
+            </React.Fragment>
         );
     };
 };
