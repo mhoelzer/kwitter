2dcd228593ade768d57956d1850b3b038646f929
Michelle Hoelzer <hoelzermm@gmail.com>
2018-12-04 12:14:56 -0500

    register stuff started but needs wiork

--- src/Actions/actions.js
+++ src/Actions/actions.js
@@ -1,5 +1,8 @@
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const REGISTER = "REGISTER";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 
 export const loginFailure = () => {
     return {
@@ -13,3 +16,60 @@ export const loginSuccess = (token) => {
         payload: token
     }
 };
+
+// without default need samename in register
+// redux thunk = middleware (like express stuff where mw got reqs to go through it. here, each action goes through there(diaspatching thigng called register from reg.js and returns function (once return = inner function; redux sees it wants action obj, so inject dispatch in))); function inside function; when have action creator, it will inject dispatch for you and get registation data
+// sees value is action obj, not function, so pass dispatch value; could also just do the simple ones (returns action obj when returns obj likr the type:...), even with thunk but that isnt async
+export const register = registerData => dispatch => {
+    // dispatch b4 fetch b/c have comp that wants to render loading spinner or something about waitingo n api req
+    dispatch({
+        type: REGISTER
+    })
+    // set method to post b/c not defautl get 
+    // if cant communicate, it will throw; else it wont know when to throw or not; fetch doesnt know
+    fetch("https://kwitter-api.herokuapp.com/auth/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            // cant just send reg js stuff; need to json it
+            // typed as how api expects them
+            // make sure pass these values  
+            // username,
+            // displayName,
+            // password
+            registerData
+        })
+    })
+        // hydrates body; always need for api b/c always returns json; if not, it would  be parsed diff
+        // .then(res => res.json()) // if immediately hydrate, it always assumes its correct, so ave to check
+        .then(res => {
+            // res.ok means it's in the 200 range; 
+            if (!res.ok) {
+                // throw from here, it goes to catch
+                res.json().then(err => {
+                    throw err;
+                });
+            }
+            return res.json(); // this is an else basically 
+        })
+        .then(data => {
+            // data looks like w/e we got for resposne in api/psotman 
+            // success already has values in state
+            // no setstate cause not in comp
+            // do dispatch on success
+            dispatch({
+                type: REGISTER_SUCCESS,
+                // add on extra data,  which is reuslt back from api (which is the username and displayname that you get back)
+                register: data,
+                result: "Successfully Registered!" // could make a <div>{this.props.result}</div> to display; reducer might use this value to put some  update
+            })
+        }).catch(err => {
+            // dispatch here on fail
+            dispatch({
+                type: REGISTER_FAIL,
+                result: "Failed to register" // get api err; usually user facing err
+            })
+        })
+}
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,14 +1,95 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { register } from "../Actions/actions.js";
 
 class Register extends Component {
+    state = {
+        username: "",
+        displayName: "",
+        password: "",
+        // result: "" // say if failed or success; dont need b/c inside action 
+    };
+
+    // function will go on dom, so need to bind correctly
+    handleUsernameChange = event => {
+        this.setState({
+            username: event.target.value
+        })
+    };
+    handleDisplayNameChange = event => {
+        this.setState({
+            displayName: event.target.value
+        })
+    };
+    handlePasswordChange = event => {
+        this.setState({
+            password: event.target.value
+        })
+    };
+    // if has same username, api will send an error; get list of all usernames and check forrepeats and do onBlur (when exiting that inout box; might need another endpoint to send username)
+    // need to think about success and error cases
+    // the fetch will have to be in redux when get there 
+    // register would be connected through action creator b/c the event isnt being used in handler, so this code doesnt have to be connected inside this comp and can move in avtion creator
+    handleRegister = event => {
+        // translate state value to get this then put elsewhere
+        this.props.register({
+            username: this.state.username,
+            displayName: this.state.displayName,
+            password: this.state.password
+        })
+    }
+
     render() {
+        // const { username, password, displayName } = this.state;
         return (
-            <div>Register</div>
+            <fieldset>
+                <legend>Register</legend>
+                <p>Username:
+                    {/* make sure to add the onchanges otherwise value wont work; record value whenever changes nad display vlaue when rerenders */}
+                    {/* required means it has to have something in it */}
+                    {/* value is making things customized/changing thinhs in state; without, dom controls how value is displayed */}
+                    <input required placeholder="Username" type="text" className="loginInputs" autoFocus onChange={this.handleUsernameChange}></input>
+                </p>
+                <p>Display Name:
+                    {/* make sure to add the onchanges otherwise value wont work */}
+                    <input required placeholder="Display Name" type="text" className="loginInputs" onChange={this.handleDisplayNameChange}></input>
+                </p>
+                <p>Password:
+                    {/* could make change/see it based on checkbox */}
+                    <input required placeholder="Password" type="password" className="loginInputs" onChange={this.handlePasswordChange}></input>
+                </p>
+                {/* change the tp="" if needed */}
+                {/* <Link to="/profile"> */}
+                    <button onClick={this.handleRegister}>Create Your New Account!</button>
+                    <div>{this.props.result}</div>
+                {/* </Link> */}
+            </fieldset>
         );
     };
 };
 
-export default Register;
+// listne to state value to rerender whne changes
+const mapStateToProps = (state) => {
+    return {
+        result: state.registerResult
+    }
+}
+// thing get in is dispatch and reutnr props
+const mapDispatchToProps = (dispatch) => {
+    // const registerData = {
 
+    // }
+    return {
+        // neesd all state stuff to create action thing
+        // register: (username, displayName, password) => dispatch(register(username, displayName, password))
+        register: (registerData) => dispatch(register(registerData))
+    }
+}
+
+// export default Register;
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
 
 // cancel button clears info?
+// could add in validations? 
+// confirm password by being exactly the same?
