965697d5a6d7aff4d3e7b1776b94f247b009adb9
morthos <pathshan61@gmail.com>
2018-12-10 10:20:11 -0500

    .

--- src/Components/EditProfile.js
+++ src/Components/EditProfile.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Form } from 'semantic-ui-react'
+
+const options = [
+  { key: 'm', text: 'Male', value: 'male' },
+  { key: 'f', text: 'Female', value: 'female' },
+]
 
 class EditProfile extends Component {
-    render(){
-        return(
-            <div>Edit profile page</div>
-        )
-    }
+  state = {}
+
+  handleChange = (e, { value }) => this.setState({ value })
+
+  render() {
+      
+    return (
+      <Form>
+        <Form.Group widths='equal'>
+          <Form.Input fluid label='First name' placeholder='First name' />
+          <Form.Input fluid label='Last name' placeholder='Last name' />
+          <Form.Select fluid label='Gender' options={options} placeholder='Gender' />
+        </Form.Group>
+
+      </Form>
+    )
+  }
 }
+
 export default EditProfile
