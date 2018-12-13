43a2cb1727fc8b61d2e40d569d7d53102f7be4dd
Michelle Hoelzer <hoelzermm@gmail.com>
2018-12-13 14:52:01 -0500

    did more styling on messages

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -7,13 +7,30 @@ class Message extends Component {
   render() {
     return (
       <Fragment>
-        {/* //   place props in styling, from fragment section. */}
-        <div>{this.props.text}</div>
-        <div>Kweeted by {this.props.username}</div>
-        <button onClick={this.props.toggleLike}>
-          {this.props.isLiked ? "Unlike" : "Like"}
-        </button>
-        <p>Likes: {this.props.numOfLikes}</p>
+        <Card
+          color="green"
+          animation="overlay"
+          Icon="labeled"
+          Inverted
+          vertical
+          width="thin"
+          fluid
+        >
+          <Card.Content>
+            <Card.Header>{this.props.text}</Card.Header>
+            {/* //   place props in styling, from fragment section. */}
+            <Card.Description>
+              Kweeted by {this.props.username}
+            </Card.Description>
+            <Divider />
+            <Card.Content extra>
+              <button onClick={this.props.toggleLike}>
+                {this.props.isLiked ? "Unlike" : "Like"}
+              </button>
+              <p>Likes: {this.props.numOfLikes}</p>
+            </Card.Content>
+          </Card.Content>
+        </Card>
       </Fragment>
     );
   }
